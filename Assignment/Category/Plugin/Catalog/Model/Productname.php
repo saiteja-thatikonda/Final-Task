7fce65f4466408b9703ac87606c70bf2826daf40
saiteja-thatikonda <90679965+saiteja-thatikonda@users.noreply.github.com>
2022-09-02 19:49:27 +0530

    Update Productname.php

--- Assignment/Category/Plugin/Catalog/Model/Productname.php
+++ Assignment/Category/Plugin/Catalog/Model/Productname.php
@@ -3,29 +3,27 @@
 namespace Assignment\Category\Plugin\Catalog\Model;
 class Productname
 {
-    public function afterGetName(\Magento\Catalog\Model\Product $subject, $name)
-     {
-        return "Gadget".$name;                                      // return the name with category name as prefix 
+    
+     
+        
+        public function __construct
+        ( \Magento\Framework\Registry $registry, array $data = [] ) 
+        
+        { $this->_registry = $registry; } 
+        
+        public function getCurrentCategory() 
+        { 
+        return $this->_registry->registry('current_category'); 
+        } 
+        
+        public function afterGetName(\Magento\Catalog\Model\Product $subject, $name) 
+        {
+         
+         $getcat= $this->getCurrentCategory(); 
+         return $getcat->getName(). "-" .$name;
+        
+        }        
        
-    }
+    
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
